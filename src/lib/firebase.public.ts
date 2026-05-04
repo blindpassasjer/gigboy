ec68fe6e09497a6646474d299009d4f9d9a128e5
blindpassasjer <sebastian@manriquez.no>
2026-05-04 21:44:18 +0200

    new domain

--- src/lib/firebase.public.ts
+++ src/lib/firebase.public.ts
@@ -1,6 +1,6 @@
 export const PUBLIC_FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA2anSovYmXrjU8fnq15_y6cfCUY_JD73M',
-  authDomain: 'songbook-bebd5.firebaseapp.com',
+  authDomain: 'gigboy.no',
   projectId: 'songbook-bebd5',
   storageBucket: 'songbook-bebd5.firebasestorage.app',
   messagingSenderId: '179160244928',
