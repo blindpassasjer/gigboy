ea204d7cea46f03db9c3b4a24016dd6ad3f09878
blindpassasjer <sebastian@manriquez.no>
2026-06-09 08:50:33 +0200

    tab player real guitar

--- src/lib/midiPlayer.ts
+++ src/lib/midiPlayer.ts
@@ -14,17 +14,18 @@ const BASE_URL =
   'https://nbrosowsky.github.io/tonejs-instruments/samples/guitar-acoustic/';
 
 // Spread of samples covering the full guitar range (E2–D5)
+// Keys use Tone.js note format (# for sharps); filenames use "s" suffix (e.g. As2.mp3)
 const SAMPLE_URLS: Record<string, string> = {
-  D2: 'D2.mp3', Ds2: 'Ds2.mp3', E2: 'E2.mp3', F2: 'F2.mp3',
-  Fs2: 'Fs2.mp3', G2: 'G2.mp3', Gs2: 'Gs2.mp3',
-  A2: 'A2.mp3', As2: 'As2.mp3', B2: 'B2.mp3',
-  C3: 'C3.mp3', Cs3: 'Cs3.mp3', D3: 'D3.mp3', Ds3: 'Ds3.mp3',
-  E3: 'E3.mp3', F3: 'F3.mp3', Fs3: 'Fs3.mp3', G3: 'G3.mp3', Gs3: 'Gs3.mp3',
-  A3: 'A3.mp3', As3: 'As3.mp3', B3: 'B3.mp3',
-  C4: 'C4.mp3', Cs4: 'Cs4.mp3', D4: 'D4.mp3', Ds4: 'Ds4.mp3',
-  E4: 'E4.mp3', F4: 'F4.mp3', Fs4: 'Fs4.mp3', G4: 'G4.mp3', Gs4: 'Gs4.mp3',
-  A4: 'A4.mp3', As4: 'As4.mp3', B4: 'B4.mp3',
-  C5: 'C5.mp3', Cs5: 'Cs5.mp3', D5: 'D5.mp3',
+  D2: 'D2.mp3', 'D#2': 'Ds2.mp3', E2: 'E2.mp3', F2: 'F2.mp3',
+  'F#2': 'Fs2.mp3', G2: 'G2.mp3', 'G#2': 'Gs2.mp3',
+  A2: 'A2.mp3', 'A#2': 'As2.mp3', B2: 'B2.mp3',
+  C3: 'C3.mp3', 'C#3': 'Cs3.mp3', D3: 'D3.mp3', 'D#3': 'Ds3.mp3',
+  E3: 'E3.mp3', F3: 'F3.mp3', 'F#3': 'Fs3.mp3', G3: 'G3.mp3', 'G#3': 'Gs3.mp3',
+  A3: 'A3.mp3', 'A#3': 'As3.mp3', B3: 'B3.mp3',
+  C4: 'C4.mp3', 'C#4': 'Cs4.mp3', D4: 'D4.mp3', 'D#4': 'Ds4.mp3',
+  E4: 'E4.mp3', F4: 'F4.mp3', 'F#4': 'Fs4.mp3', G4: 'G4.mp3', 'G#4': 'Gs4.mp3',
+  A4: 'A4.mp3', 'A#4': 'As4.mp3', B4: 'B4.mp3',
+  C5: 'C5.mp3', 'C#5': 'Cs5.mp3', D5: 'D5.mp3',
 };
 
 function getTone(): Promise<ToneModule> {
