0c87cc0e695c252b91d4faf559640d7844237947
blindpassasjer <sebastian@manriquez.no>
2026-07-15 21:08:06 +0200

    scroll fix

---
+++ android/app/src/main/java/com/gigboy/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gigboy.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gigboy.app',
+  appName: 'GIGBOY',
+  webDir: 'dist'
+};
+
+export default config;
